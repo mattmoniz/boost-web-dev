0bc55bbce08f519360ce92fbd3aad362cb2671d3
Matthew Moniz <Matt@Fastforward.sh>
2024-02-02 16:47:46 -0500

    more updates to packages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,22 +28,22 @@ const Contact = (props) => (
                     <div className="contact-method">
                         <span className="icon alt fa-envelope"></span>
                         <h3>Email</h3>
-                        <a href="#">information@untitled.tld</a>
+                        <a href="#">mattbwd@gmail.com</a>
                     </div>
                 </section>
-                <section>
+                {/* <section>
                     <div className="contact-method">
                         <span className="icon alt fa-phone"></span>
                         <h3>Phone</h3>
                         <span>(000) 000-0000 x12387</span>
                     </div>
-                </section>
+                </section> */}
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-home"></span>
                         <h3>Address</h3>
-                        <span>1234 Somewhere Road #5432<br />
-                        Nashville, TN 00000<br />
+                        <span>6 Claire Ave<br />
+                        Mansfield, MA 02048<br />
                         United States of America</span>
                     </div>
                 </section>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,10 +31,10 @@ const Footer = props => (
         </li>
       </ul>
       <ul className="copyright">
-        <li>&copy; Untitled</li>
-        <li>
+        {/* <li>&copy; Untitled</li> */}
+        {/* <li>
           Design: <a href="https://html5up.net">HTML5 UP</a>
-        </li>
+        </li> */}
       </ul>
     </div>
   </footer>
