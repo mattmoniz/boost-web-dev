423986b0dd9e6f0de40a9fccefb90c7a600dc602
Matthew Moniz <Matt@Fastforward.sh>
2024-02-02 20:12:55 -0500

    updated header

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,12 +7,12 @@
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
-}
+// exports.createPages = async ({ actions }) => {
+//   const { createPage } = actions
+//   createPage({
+//     path: "/using-dsg",
+//     component: require.resolve("./src/templates/using-dsg.js"),
+//     context: {},
+//     defer: true,
+//   })
+// }
