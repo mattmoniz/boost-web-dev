3f3fcf6c404ba91d854a049da962c68d9e20b117
Matthew Moniz <Matt@Fastforward.sh>
2024-02-15 20:05:39 -0500

    removed unused variables

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
-import pic11 from '../assets/images/pic11.jpg'
 
 const Generic = props => (
   <Layout>
